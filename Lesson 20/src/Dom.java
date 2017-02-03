9c2fe191c421e49d6b6ce7f0ed7f0fb58c2c3fcb
Ingvarrr <falcon.only@gmail.com>
2017-02-03 14:13:11 +0200

    Finished DOM parsing of XML file

--- Lesson 20/src/Dom.java
+++ Lesson 20/src/Dom.java
@@ -1,4 +1,5 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -16,6 +17,12 @@ public class Dom {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse("Lesson 20/src/person.xml");
+
+        document.getDocumentElement().normalize();
+
+        System.out.println("Root Element: " + document.getDocumentElement().getNodeName() + ", id = "
+                + document.getDocumentElement().getAttribute("id"));
+
         NodeList nodeList = document.getDocumentElement().getChildNodes();
 
         System.out.println();
@@ -23,8 +30,44 @@ public class Dom {
         for (int i = 0; i<nodeList.getLength(); i++){
             Node node = nodeList.item(i);
             if(node.getNodeType() == Node.ELEMENT_NODE){
+
+                Element el = (Element) node;
+
+                if("name".equals(node.getNodeName())){
+                    System.out.println("Name is : ");
+                    for (int j = 0; j < node.getChildNodes().getLength(); j++) {
+                        Node node1 = node.getChildNodes().item(j);
+                        if ("firstName".equals(node1.getNodeName())) {
+                            System.out.println("FirstName " + node1.getFirstChild().getNodeValue());
+                        }
+                        if ("lastName".equals(node1.getNodeName())) {
+                            System.out.println("LastName " + node1.getFirstChild().getNodeValue());
+                        }
+                    }
+                }
                 if("login".equals(node.getNodeName())){
                     System.out.println("Login is " + node.getFirstChild().getNodeValue());
+                }
+                if("permission".equals(node.getNodeName())){
+                    System.out.println("Permission :");
+                    for (int j = 0; j < node.getChildNodes().getLength(); j++) {
+                        Node node1 = node.getChildNodes().item(j);
+                        if ("document".equals(node1.getNodeName())) {
+                            System.out.println("Document " + node1.getFirstChild().getNodeValue());
+                        }
+                        if ("pages".equals(node1.getNodeName())) {
+                            System.out.println("Pages " + node1.getFirstChild().getNodeValue());
+                        }
+                    }
+                }
+                if("documents".equals(node.getNodeName())){
+                    System.out.println("Documents :");
+                    for (int j = 0; j < node.getChildNodes().getLength(); j++) {
+                        Node node1 = node.getChildNodes().item(j);
+                        if ("doc".equals(node1.getNodeName())) {
+                            System.out.println("Document " + node1.getFirstChild().getNodeValue());
+                        }
+                    }
                 }
             }
         }
